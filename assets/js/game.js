2fdc734848b809974d77566e2a29244c3eaab541
JoKing <joebryant614@gmail.com>
2021-03-23 22:51:45 -0700

    add enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,21 @@
+// Game States 
+// "WIN" - Player robot has defeated all enemy-robots 
+//      * Fight all enemy-robots 
+//      *Defeat each enemy-robot
+// "Loss" - Player robot's health is zero or less
+
 // Add name prompt 
 var playerName = window.prompt("What is your robot's name?")
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth, playerMoney);
-
-var enemyName = "Morgana"
+var enemyNames = ["Morgana", "Master Yi", "Yumi"];
 var enemyHealth = 50;
 var enemyAttack = 10;
 
-var fight = function () {
+
+var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators")
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
@@ -67,4 +71,6 @@ var fight = function () {
 
 }
 
-fight()
+for(var i = 0; i < enemyNames.length; i++) { 
+    fight(enemyNames[i]);
+}
